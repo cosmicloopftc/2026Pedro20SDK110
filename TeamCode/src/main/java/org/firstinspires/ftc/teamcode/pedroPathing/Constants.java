17c4feaece2b79f71e936728a9a44fbde3cf559e
BaronClaps <126834072+baronclaps@users.noreply.github.com>
2025-09-26 21:12:11 -0500

    9/28/2025: WT: Convert old robot Constants and carry over to here.
    2.0.2 Pedro FTC + 1.0.0 Pedro Telemetry + 1.0.6 Full Panels
    
    ---
    
    Add Desired Robot Pose + Path Drawing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,19 +1,95 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+/** 9/28/2025 convert old Pedropath LConstants and
+ *      Fconstants from 2/1/2025 IntoTheDeep Robot to Constants here.
+
+
+ */
+
+
+
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(13.3)
+            .forwardZeroPowerAcceleration(-31)
+            .lateralZeroPowerAcceleration(-69)
+            .useSecondaryTranslationalPIDF(false)
+            .useSecondaryHeadingPIDF(false)
+            .useSecondaryDrivePIDF(true)
+            .centripetalScaling(0.0005)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
+            .drivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.06, 0, 0.0015, 0.6, 0)
+            )
+            .secondaryDrivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.008, 0, 0.0001, 0.6, 0)
+            );
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .leftFrontMotorName("leftFront")
+            .leftRearMotorName("leftRear")
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftFrontMotorDirection(DcMotorEx.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorEx.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorEx.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD)
+            .xVelocity(60)
+            .yVelocity(41.5);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(5.25)
+            .strafePodX(-1.25)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(
+                    GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD
+            )
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            500,
+            1,
+            1
+    );
+
+    //new for PedroPath 2.0.2:  public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    /** OLD PedroPath:
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .build();
+    }
+    **/
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
