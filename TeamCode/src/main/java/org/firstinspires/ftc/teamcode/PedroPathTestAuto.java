477185cfee414c476e9b94bda8c213531a17d6a0
DominicACP <dr.dominic903@gmail.com>
2026-01-18 21:07:01 -0500

    PedroPath auto test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathTestAuto.java
@@ -11,7 +11,7 @@ import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "Example Auto", group = "Examples")
+@Autonomous(name = "Test Auto", group = "Examples")
 public class PedroPathTestAuto extends OpMode {
 
     private Follower follower;
