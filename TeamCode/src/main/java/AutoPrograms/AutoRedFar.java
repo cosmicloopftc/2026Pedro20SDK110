6d574d392cc1ba4f51cfcaa6c3ed921427468cac
DominicACP <dr.dominic903@gmail.com>
2026-02-05 19:52:58 -0500

    Added teleop to auto support

--- TeamCode/src/main/java/AutoPrograms/AutoRedFar.java
+++ TeamCode/src/main/java/AutoPrograms/AutoRedFar.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode; // make sure this aligns with class location
+package AutoPrograms; // make sure this aligns with class location
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
