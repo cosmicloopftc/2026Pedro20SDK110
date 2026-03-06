9d7a37714119530ef6dcaf883fb4b68284b9414d
DominicACP <dr.dominic903@gmail.com>
2026-03-06 18:01:18 -0500

    3/8/2026: OT--completed Turret aim by PedroPath--completed but commented out for now: line 294-298, and using Limelight line 279-291 MT: updated shootin and intake with new intake wheels
    3/8/2026 post Scrimmage 2 at Spartans
    OT: turret aiming by Pedropathing--NOT done
    
    2/28/2026:
    AT--add auto align turret by PD feed forward
    MT--starting to program new spindexer and digital color sensor
    
    2/17/2026: add Sparkfun LED stick and imu test
    (note this branch is before Qualifier 2.
    
    Feb 15: add 3rd LED to teleOp; sorting Teleop-need to test; add turret to aim to 0 degee when drivetrain is btw -45,+45 AND no Limelight detection.  NEED TO TEST THESE.
    
    Feb 14, Add LEDs to teleop, sorting but not working
    
    Feb 14, Sat, start of day, duplicate from Feb13 version
    
    Feb 13: by MT up to Friday night, 2/13/2026--color sensor
    
    Tuned Pedropath 2.0
    
    Added teleop to auto support

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RED_TeleOpV2.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware.HardwareMain;
+
+import java.util.List;
+
+@Configurable
+@TeleOp(name= "RED_TeleOpV2 V1.3", group = "RED")
+
+public class RED_TeleOpV2 extends BLUE_TeleOpV2 {
+
+    //int autoPipeline;    // TODO: set pipeline to Red
+
+
+    @Override
+    public void init() {
+        //TODO: need to adjust this starting point--for now it is at BLUE endgame box--robot and turret point straight forward.
+        //TODO: set startingPose to start TeleOp--tx from Auto?
+        //blue Endgame box = (105, 33, ...); //red Endgame box = (38, 33, ....)
+        //TODO: need to confirm with Dominic:  BLUE goal ?? (14,128);   RED goal (129, 128);
+        //For now, at start of TeleOp, set RED FAR robot to be at BLUE Endgame box = (38. 33, ....) and point up to goal Y direction
+        //For now, at start of TeleOp, set RED NEAR robot to be at ??? and point up to goal Y direction
+
+        autoPipeline = 0;  // TODO: set pipeline to Red
+        GoalX = 129;                    //RED goal coordinates
+        GoalY = 128;
+        startingPose = new Pose(105,33, Math.toRadians(90)); //    Consider having the robot at this BLUE endgame box area.
+        super.init();
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+    }
+
+
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+    }
+
+    @Override
+    public void stop(){
+        super.stop();
+    }
+}
